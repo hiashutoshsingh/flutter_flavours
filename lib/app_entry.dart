7d1c555171a143d7a13057e0a87aa38b4d3a07b9
Ashutosh Singh <ashutoshsingh.0207@gmail.com>
2021-06-02 16:34:57 +0530

    home screen migration

--- lib/app_entry.dart
+++ lib/app_entry.dart
@@ -2,7 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flavour/config.dart';
 import 'package:flavour/push_notifications.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+
+import 'home.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -30,56 +31,6 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _bundledId = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _getPhoneInfo();
-  }
-
-  Future<void> _getPhoneInfo() async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    String packageName = packageInfo.packageName;
-    setState(() {
-      _bundledId = packageName;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              _bundledId + '\n' + Config.appFlavor.toString(),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _bundledId = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _getPhoneInfo();
+  }
+
+  Future<void> _getPhoneInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String packageName = packageInfo.packageName;
+    setState(() {
+      _bundledId = packageName;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _bundledId + '\n' + Config.appFlavor.toString(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
